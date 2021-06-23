5a0f3ee427389bc89291aa57df9d6e86398bed18
Kit4Whale <60572662+Kit4Whale@users.noreply.github.com>
2021-06-23 23:15:22 +0300

    Добавлено несколько функций

--- sendbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sendbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,27 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 	public static void main (String [] args) {
-		System.out.println("Hello, world!");
+		hello("world");
+		hello("user");
+		hello("Nikita");
+
+		double len = 5;
+		System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+		double q = 2;
+		double w = 4;
+		System.out.println("Площадь прямоугольника со сторнами " + q + " и " + w + " = " + area(q, w));
+	}
+
+	public static void hello (String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area (double l) {
+		return l * l;
+	}
+
+	public static double area (double a, double b) {
+		return a * b;
 	}
 }
